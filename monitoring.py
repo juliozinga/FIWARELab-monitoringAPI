9a0439e122d478639296a01e43fe828e9181e3a9
Daniel Depaoli <daniel.depaoli@create-net.org>
2016-02-09 12:19:55 +0100

    Add authentication IDM part

--- monitoring.py
+++ monitoring.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python
 
-from bottle import route, run, request, error, response, Bottle
+from bottle import route, run, request, error, response, Bottle, redirect, HTTPError, abort
 from pymongo import MongoClient, database
 from bottle.ext.mongo import MongoPlugin
 from bson.json_util import dumps
 import argparse
 import ConfigParser
 import sys
+import time
+import json
+import urllib2
 
+#Main bottle app
 app = Bottle()
-COLLECTION = 'entities'
 
-@error(404)
+def get_keypass_token(username, password, url):
+    data = { 'auth': {'identity': { 'methods': ['password'],
+    'password': { 'user': { 'name': username , 'domain': { 'id': 'default' },
+    'password': password } } } }}
+
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/json')
+    response = urllib2.urlopen(req, json.dumps(data))
+    accepted_response = [200, 201, 202]
+    if response.getcode() in accepted_response:
+        return response.headers.get('X-Subject-Token')
+    else:
+        return None
+
+def request_to_idm(username, password, keypass_url, url):
+    token = get_keypass_token(username=username, password=password, url=keypass_url)
+    req = urllib2.Request(url)
+    if token:
+        req.add_header('X-Auth-Token', token)
+        response = urllib2.urlopen(req)
+        return response
+    return None
+
+@app.error(404)
 def error404(error):
     response.content_type = 'application/json'
     return {'Request not found'}
+
+@app.error(401)
+def error401(error):
+    response.content_type = 'application/json'
+    return {'UNAUTHORIZED'}
 
 @app.route('/')
 def root(mongodb):
@@ -43,10 +74,10 @@ def get_all_regions(mongodb):
     basicUsers = 0
     trialUsers = 0
     communityUsers = 0
-    totalUsers = 0;
+    totalUsers = 0
     total_nb_users = 0 #backward compatibility*/
     totalUserOrganizations = 0
-    totalCloudOrganizations = 0                                                                                                                                                                                            
+    totalCloudOrganizations = 0
     total_nb_organizations = 0.0 #backward compatibility*/
     total_nb_cores = 0.0
     total_nb_cores_enabled = 0.0
@@ -57,15 +88,15 @@ def get_all_regions(mongodb):
     total_ip_allocated = 0
     total_ip = 0
     tmp_reg = []
-    tmp_res={"_links": {"self": { "href": "" }},"measures":[{}]}
+    tmp_res={"_links": {"self": { "href": "" }},"measures":[{}], "_embedded":{"regions":[]} }
     particular_region = ["Berlin", "Stockholm2", "SaoPaulo", "Mexico"]
 
     since = None
     if request.query.getone('since') is not None:
         since = request.query.getone('since')
 
-    #import pdb; pdb.set_trace()
     result = mongodb['entities'].find({"_id.type":"region"})
+    now = int(time.time())
     for region in result:
         if region["_id"]["id"] not in particular_region:
             href_txt = "/monitoring/regions/"+region["_id"]["id"]
@@ -109,11 +140,12 @@ def get_all_regions(mongodb):
                         #if now-regions[i].modDate<cfgObj.regionTTL:
                             total_nb_disk+=int(float(att["value"]))
 
-
                 if att["name"] and att["name"].find("vmList") != -1:
-                    total_vms = total_vms + 1
+                    for vm in att["value"].split(';'):
+                        if vm.find('ACTIVE'):
+                            total_vms = total_vms + 1
 
-    tmp_res["_embedded"]["region"].append(tmp_reg)
+    tmp_res["_embedded"]["regions"].append(tmp_reg)
     tmp_res["total_nb_vm"] = total_vms
     tmp_res["basicUsers"] = basicUsers
     tmp_res["trialUsers"] = trialUsers
@@ -129,6 +161,61 @@ def get_all_regions(mongodb):
     tmp_res["total_nb_disk"] = total_nb_disk
     tmp_res["total_ip_assigned"] =  total_ip_used
     tmp_res["total_ip_allocated"] =  total_ip_allocated
+    tmp_res["total_ip"] = total_ip
+
+    #Authentication part
+    try:
+        idm_body = request_to_idm(username=app.config.get("idm_username"),
+                                password=app.config.get("idm_password"),
+                                keypass_url=app.config.get("token_url"),
+                                url=app.config.get("service_url"))
+        idm_body_json = json.loads(idm_body.read())
+    except Exception as e:
+        abort(401)
+
+    if idm_body_json["information"]["basicUsers"]:
+        basicUsers = idm_body_json["information"]["basicUsers"]
+    if idm_body_json["information"]["trialUsers"]:
+        trialUsers = idm_body_json["information"]["trialUsers"]
+    if idm_body_json["information"]["communityUsers"]:
+        communityUsers = idm_body_json["information"]["communityUsers"]
+    if idm_body_json["information"]["totalUsers"]:
+        totalUsers = idm_body_json["information"]["totalUsers"]
+    if idm_body_json["information"]["totalCloudOrganizations"]:
+        totalCloudOrganizations = idm_body_json["information"]["totalCloudOrganizations"]
+    if idm_body_json["information"]["totalUserOrganizations"]:
+        totalUserOrganizations = idm_body_json["information"]["totalUserOrganizations"]
+
+    total_nb_users = basicUsers + trialUsers + communityUsers
+    total_nb_organizations = totalUserOrganizations + totalCloudOrganizations
+
+    if len(tmp_reg) > 0:
+        _embedded={}
+        _embedded["regions"] = tmp_reg
+        tmp_res["_embedded"] = _embedded
+
+    tmp_res["basicUsers"] = basicUsers
+    tmp_res["trialUsers"] = trialUsers
+
+    tmp_res["communityUsers"] = communityUsers
+    tmp_res["totalUsers"] = totalUsers
+
+    tmp_res["total_nb_users"] = total_nb_users
+    tmp_res["totalCloudOrganizations"] = totalCloudOrganizations
+
+    tmp_res["totalUserOrganizations"] = totalUserOrganizations
+    tmp_res["total_nb_organizations"] = total_nb_organizations
+
+    tmp_res["total_nb_cores"] = total_nb_cores
+    tmp_res["total_nb_cores_enabled"] = total_nb_cores_enabled
+
+    tmp_res["total_nb_ram"] = total_nb_ram
+    tmp_res["total_nb_disk"] =total_nb_disk
+
+    tmp_res["total_nb_vm"] = total_nb_vm
+    tmp_res["total_ip_assigned"] = total_ip_used
+
+    tmp_res["total_ip_allocated"] = total_ip_allocated
     tmp_res["total_ip"] = total_ip
 
     return tmp_res
@@ -263,8 +350,18 @@ def main():
         print("Error in mongodb: {}").format(e)
         sys.exit(-1)
 
+    try:
+        app.config["token_url"] = ConfigSectionMap('idm', Config)['token_url']
+        app.config["service_url"] = ConfigSectionMap('idm', Config)['service_url']
+        app.config["idm_username"] = ConfigSectionMap('idm', Config)['username']
+        app.config["idm_password"] = ConfigSectionMap('idm', Config)['password']
+    except Exception as e:
+        print("Error in IDM config: {}").format(e)
+        sys.exit(-1)   
+
     #dumps(mongodb['entities'].find({"_id.type":"vm", "_id.id":{'$regex':'Trento'}}))
     app.install(plugin)
+
     run(app, host=listen_url, port=listen_port, debug=True)
 
 if __name__ == '__main__':
