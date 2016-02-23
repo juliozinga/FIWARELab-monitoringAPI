118a4476357f42b50121ac7dd1bf8c41e47ee1f7
Daniel Depaoli <daniel.depaoli@create-net.org>
2016-02-23 10:29:44 +0100

    Print Bearer and x-auth-token curl request version

--- get_token.py
+++ get_token.py
@@ -2,6 +2,7 @@
 
 from proxy_monitoring import get_token_auth
 import sys
+import base64
 
 URL = "https://account.lab.fiware.org/oauth2/token"
 
@@ -12,9 +13,12 @@ if __name__ == '__main__':
         print sys.argv[0] + " <username> <password> <url> <consumer_key> <consumer_secret>"
         sys.exit(-1)
     try:
-        token64 = get_token_auth(username=sys.argv[1], password=sys.argv[2], url=URL, consumer_key=sys.argv[3], consumer_secret=sys.argv[4])
-        print "\nYour token is: {}\n".format(token64)
+        token = get_token_auth(username=sys.argv[1], password=sys.argv[2], url=URL, consumer_key=sys.argv[3], consumer_secret=sys.argv[4], convert_to_64=False)
+        token64 = base64.b64encode(token)
+        print "\nYour token id is: (base32){} (base64){}\n".format(token, token64)
         print "Use it in this way:"
         print "curl -H \"Authorization:Bearer {}\" -s <monitoring_ip>:<monitoring_port>/monitoring/regions".format(token64)
+        print "or :"
+        print "curl -H \"X-Auth-Token: {}\" -s <monitoring_ip>:<monitoring_port>/monitoring/regions".format(token)
     except Exception as e:
         print "Impossible to get token: " + str(e)
