e4a467236d7a07d9fa90635e15800d868d9e9b93
Daniele Santoro <psyacca@gmail.com>
2016-05-24 13:28:42 +0200

    Database schema changes
    - Organize DB schemas and alter files
    - Remove current timestamp on update in host and region tables

---
+++ monitoringHisto/db/monitoring/monitoring_alter_v1.0_v1.1.sql
@@ -0,0 +1,19 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+USE `monitoring`;
+
+-- -----------------------------------------------------
+-- Table `monitoring`.`host` alter
+-- -----------------------------------------------------
+ALTER TABLE `monitoring`.`host` MODIFY `timestampId` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- -----------------------------------------------------
+-- Table `monitoring`.`region` alter
+-- -----------------------------------------------------
+ALTER TABLE `monitoring`.`region` MODIFY `timestampId` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ monitoringHisto/db/monitoring_new/monitoring_new_alter_v1.0_v1.1.sql
@@ -0,0 +1,19 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+USE `monitoring_new`;
+
+-- -----------------------------------------------------
+-- Table `monitoring_new`.`host` alter
+-- -----------------------------------------------------
+ALTER TABLE `monitoring_new`.`host` MODIFY `timestampId` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- -----------------------------------------------------
+-- Table `monitoring_new`.`region` alter
+-- -----------------------------------------------------
+ALTER TABLE `monitoring_new`.`region` MODIFY `timestampId` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
