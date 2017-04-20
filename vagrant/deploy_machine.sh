e16800ba47fab470a2d787399e912c3c99704415
Daniele Santoro <dsantoro@create-net.org>
2017-04-20 23:34:30 +0200

    Improve provisioning script and add python via pyenv

--- vagrant/deploy_machine.sh
+++ vagrant/deploy_machine.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
+#set -e
+
 MY_PWD='r00tme'
-YUM_INSTALL='yum install -y '
+YUM_INSTALL='sudo yum install -y '
+RPM_INSTALL='sudo rpm -ivh --force '
 
 echo Provisioning...
-yum update -y
+sudo yum update -y
 echo -e "[mongodb-org-2.6]\nname=MongoDB 2.6 Repository\nbaseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/\ngpgcheck=0\nenabled=1"|sudo tee /etc/yum.repos.d/mongodb-org-2.6.repo
 $YUM_INSTALL mongodb-org
 $YUM_INSTALL wget
 $YUM_INSTALL screen
 $YUM_INSTALL emacs-nox
-$YUM_INSTALL python-pip.noarch
+$YUM_INSTALL git
+$RPM_INSTALL http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+#$YUM_INSTALL python-pip
 
-# Install and customize git
-yum -y install git
+# Install python 2.7 and pip with pyenv
+source /vagrant/install_pyenv.sh
+
+# Customize git
 git config --global color.ui auto
 
 #context broker install
-wget -O /tmp/epel-release-latest-6.noarch.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-rpm -i /tmp/epel-release-latest-6.noarch.rpm
-yum install -y boost-system boost-filesystem boost-thread libmicrohttpd logrotate libcurl boost-regex
-curl --insecure -o /tmp/contextBroker-0.26.1-1.x86_64.rpm https://forge.fiware.org/frs/download.php/1713/contextBroker-0.26.1-1.x86_64.rpm
-rpm -i /tmp/contextBroker-0.26.1-1.x86_64.rpm
+sudo wget --progress=bar:force -O /tmp/epel-release-latest-6.noarch.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+$RPM_INSTALL /tmp/epel-release-latest-6.noarch.rpm
+$YUM_INSTALL boost-system boost-filesystem boost-thread libmicrohttpd logrotate libcurl boost-regex
+sudo curl --insecure -o /tmp/contextBroker-0.26.1-1.x86_64.rpm https://forge.fiware.org/frs/download.php/1713/contextBroker-0.26.1-1.x86_64.rpm
+$RPM_INSTALL /tmp/contextBroker-0.26.1-1.x86_64.rpm
 
 #NodeJs install
 curl --silent --location https://rpm.nodesource.com/setup_4.x |sudo bash -
-yum -y install nodejs
-yum -y install gcc-c++ make
-yum -y install npm
+$YUM_INSTALL nodejs
+$YUM_INSTALL gcc-c++ make
+$YUM_INSTALL npm
 
-/etc/init.d/contextBroker start
-/etc/init.d/mongod start
+sudo /etc/init.d/contextBroker start
+sudo /etc/init.d/mongod start
 
 # monitoringAPI and dependencies
 git clone https://github.com/SmartInfrastructures/FIWARELab-monitoringAPI.git
@@ -47,11 +54,11 @@ if [[ -d /vagrant/SHARED/mongodb_backup/ ]]; then
 fi
 
 #Mysql
-yum -y install mysql-server
-/sbin/service mysqld start
-/usr/bin/mysqladmin -u root password $MY_PWD
-/usr/bin/mysqladmin -u root -p$MY_PWD -h localhost.localdomain password $MY_PWD
-chkconfig mysqld on
+$YUM_INSTALL mysql-server
+sudo /sbin/service mysqld start
+sudo /usr/bin/mysqladmin -u root password $MY_PWD
+sudo /usr/bin/mysqladmin -u root -p$MY_PWD -h localhost.localdomain password $MY_PWD
+sudo chkconfig mysqld on
 
 # Restore from MySQL DB dump
 if [[ -d /vagrant/SHARED/backupMysql.dump ]]; then
